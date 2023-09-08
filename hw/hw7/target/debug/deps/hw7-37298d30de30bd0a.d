c7c7ac5fa4284e477332e093bf9cad87d24ee765
Carinthorn <tthunyaphon2547@gmail.com>
2023-09-08 18:09:40 +0700

    hw71

---
+++ hw/hw7/target/debug/deps/hw7-37298d30de30bd0a.d
@@ -0,0 +1,5 @@
+/Users/thunyaphon/Documents/GitHub/Rust/hw/hw7/target/debug/deps/hw7-37298d30de30bd0a.rmeta: src/main.rs
+
+/Users/thunyaphon/Documents/GitHub/Rust/hw/hw7/target/debug/deps/hw7-37298d30de30bd0a.d: src/main.rs
+
+src/main.rs:
